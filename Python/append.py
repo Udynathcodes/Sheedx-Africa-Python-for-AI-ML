0afcbd12bec7e23239d940a7cc7bfc708cc3fe43
Udynathcodes <uwahuduakobong@gmail.com>
2025-06-05 12:15:46 +0100

    update some files

---
+++ Python/append.py
@@ -0,0 +1,6 @@
+List1 = (1, 2, 3, 4, 5)
+tup1 = list(List1)
+tup1.remove(3)
+List1 = tuple(tup1)
+print(List1)
+
---
+++ Python/loop.py
@@ -0,0 +1,12 @@
+#tup = ("Orange", "Apple", "Banana", "Grapes")
+#for item in range(len(tup)):
+    #print(tup) 
+
+
+    #While loop example
+X = ("Sandra", "Edidiong", "Esther", "Promise")
+i = 1
+while i < len(X):
+    print(X[i])
+    i += 1
+
